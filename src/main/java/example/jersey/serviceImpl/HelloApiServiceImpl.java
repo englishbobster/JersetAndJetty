4dc796734565cafc9c4d3e3e792f86fb40f7d4e0
englishbobster <englishbobster@gmail.com>
2016-05-19 20:40:34 +0200

    Added slf4j dependency.

--- src/main/java/example/jersey/serviceImpl/HelloApiServiceImpl.java
+++ src/main/java/example/jersey/serviceImpl/HelloApiServiceImpl.java
@@ -1,25 +1,24 @@
 package example.jersey.serviceImpl;
 
-import com.google.common.collect.ImmutableList;
 import example.jersey.api.hello.HelloApiService;
-import example.jersey.model.hello.HelloModel;
 
 import javax.inject.Inject;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
-import java.util.List;
 
 public class HelloApiServiceImpl extends HelloApiService {
 
     @Inject
     HelloAdapter adapter;
 
-    List<HelloModel> hellos = ImmutableList.of(new HelloModel().sweGreet("Hej"));
 
     @Override
     public Response retrieveGreets(SecurityContext securityContext) throws NotFoundException {
         return Response.ok().entity(adapter.getGreets()).build();
-//        return Response.ok().entity(new HelloListModel().hello(hellos)).build();
+
+/* Use this to test if everything is working as it should be
+        List<HelloModel> hellos = ImmutableList.of(new HelloModel().sweGreet("Hej"));
+        return Response.ok().entity(new HelloListModel().hello(hellos)).build();*/
     }
 }
